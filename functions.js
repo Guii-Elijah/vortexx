6f2ca342dca427c5fb792b6a02df6dc363a7158b
vortex <57275178+yongvortex@users.noreply.github.com>
2020-04-02 00:10:03 +0000

    Criamos a function.js para algumas funções

---
+++ functions.js
@@ -0,0 +1,43 @@
+module.exports = {
+    getMember: function(message, toFind = '') {
+        toFind = toFind.toLowerCase();
+
+        let target = message.guild.members.get(toFind);
+        
+        if (!target && message.mentions.members)
+            target = message.mentions.members.first();
+
+        if (!target && toFind) {
+            target = message.guild.members.find(member => {
+                return member.displayName.toLowerCase().includes(toFind) ||
+                member.user.tag.toLowerCase().includes(toFind)
+            });
+        }
+            
+        if (!target) 
+            target = message.member;
+            
+        return target;
+    },
+
+    formatDate: function(date) {
+        return new Intl.DateTimeFormat('en-US').format(date)
+    },
+
+    promptMessage: async function (message, author, time, validReactions) {
+        //Colocamos o tempo em segundos, com isso sendo transferido para o MS
+        time *= 1000;
+
+        //Para cada emoji nos parâmetros de função, reaja na boa ordem.
+        for (const reaction of validReactions) await message.react(reaction);
+
+        //Permitir apenas reações do autor, 
+        //e o emoji deve estar na matriz que fornecemos.
+        const filter = (reaction, user) => validReactions.includes(reaction.emoji.name) && user.id === author.id;
+
+        // And ofcourse, await the reactions
+        return message
+            .awaitReactions(filter, { max: 1, time: time})
+            .then(collected => collected.first() && collected.first().emoji.name);
+    }
+};
